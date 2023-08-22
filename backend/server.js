483514713c050f2422e359b4bb24b744bf564066
amit-mehta28 <amitmehta7973@gmail.com>
2023-08-22 20:11:42 +0530

    kjdfsjkdfj

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 9000;
 
-app.get("/",(req,res)=>{
-    res.send("Hey I am live !!");
+const port = 9000;
+app.use(cors("*"));
+app.use(express.json());
+
+
+let arrayDatas = [];
+
+app.get("/showData", (req, res) => {
+    res.json(arrayDatas);
 })
 
-app.listen(port,()=>{
+app.post("/data", (req, res) => {
+    arrayDatas.push(req.body);
+    console.log(arrayDatas,"arrayDatas==");
+});
+
+app.listen(port, () => {
     console.log("Listning . . . ");
 })
